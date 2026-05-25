206cc4a82fb5cb8d0015a8f113fb5c7c18db0093
brandonalrodriguezpe-tech <brandonal.rodriguezpe@ecci.edu.co>
2026-05-25 10:50:45 -0500

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from miaplicación import views  # Importa las vistas de tu app
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('formulario/', views.formulario, name='formulario'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin/', admin.site.urls),
+    path('', views.catalogo_libros, name='catalogo'),               # Módulo Público
+    path('mis-prestamos/', views.mis_prestamos, name='prestamos'),    # Módulo Privado 1
+    path('nuevo-libro/', views.agregar_libro, name='agregar'),        # Módulo Privado 2
 ]
